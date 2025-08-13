db094c2f7400ebcf1ebd2d4cab2dd028334a6f02
guilletoo <165073162+guilletoo@users.noreply.github.com>
2025-08-12 22:20:31 -0300

    Update fecha.cpp
    se completó todo menos liberarTFecha

--- tarea1/src/fecha.cpp
+++ tarea1/src/fecha.cpp
@@ -8,14 +8,46 @@ struct rep_fecha
     /****** Fin de parte Parte 2.1 *****/
 };
 
+//Funciones auxiliares que voy a crear para aumentar Tfecha
+//verifica si el año es bisiesto
+bool esBisiesto(nat anio){
+    if ((anio % 400 == 0) || (anio % 4 == 0 && anio % 100 != 0)) return true; //si es multiplo de 4 y no de 100, o de 400 es bisiesto
+    
+    return false;
+
+}
+//verifica los dias de un mes dado
+static nat diasMes(nat mes, nat anio){ 
+    switch (mes) {
+    case 4 : case 6 : case 9 : case 11 : return 30;
+    case 2 : return esBisiesto(anio) ? 29 : 28;
+       
+    default: return 31;
+    }
+}
+
+static int comparador(TFecha fecha1, TFecha fecha2){
+    if (fecha1->anio != fecha2->anio) //varios if comparando gerarquicamente el año, despues el mes y por ultimo el dia
+    return (fecha1->anio > fecha2->anio) ? 1 : -1; //el ? funciona eligiendo 1 o -1 depende de el valor booleano que da lo de dentro del () 
+
+    if (fecha1->mes != fecha2->mes) 
+    return (fecha1->mes > fecha2->mes) ? 1 : -1;
+
+    if (fecha1->dia != fecha2->dia)
+    return (fecha1->dia > fecha2->dia) ? 1 : -1;
+
+    return 0; //si las fechas son iguales 
+}
+
+
 TFecha crearTFecha(nat dia, nat mes, nat anio)
 {
-    TFecha nuevaFecha = NULL;
-    /************ Parte 3.1 ************/
-    /*Escriba el código a continuación */
-
-    /****** Fin de parte Parte 3.1 *****/
+    TFecha nuevaFecha = new rep_fecha;
+    nuevaFecha->dia = dia;
+    nuevaFecha->mes = mes;
+    nuevaFecha->anio = anio;
     return nuevaFecha;
+    
 }
 
 void liberarTFecha(TFecha &fecha)
@@ -31,9 +63,12 @@ void imprimirTFecha(TFecha fecha)
     printf("%u/%u/%u\n", fecha->dia, fecha->mes, fecha->anio);
 }
 
-TFecha copiarTFecha(TFecha fecha)
-{
-    return NULL;
+TFecha copiarTFecha(TFecha fecha) //copia la fecha usando la funcion crear fecha
+{                          
+    if (fecha == NULL ) return NULL; //por si no me llegan a pasar ninguna fecha
+    else 
+    return crearTFecha(fecha->dia, fecha->mes, fecha->anio);
+
 }
 
 /************ Parte 3.9 ************/
@@ -41,18 +76,24 @@ TFecha copiarTFecha(TFecha fecha)
 /*Recuerde que las funciones auxiliares
   deben declararse antes de ser utilizadas*/
 
-void aumentarTFecha(TFecha &fecha, nat dias)
-{
+void aumentarTFecha(TFecha &fecha, nat dias) {
+
+fecha->dia += dias;
+while (fecha->dia > diasMes(fecha->mes, fecha->anio)) {
+fecha->dia -= diasMes(fecha->mes, fecha->anio);
+fecha->mes++;
+if (fecha->mes > 12) {
+fecha->mes = 1;
+fecha->anio++;
 }
+}
+}
+
 
 /****** Fin de parte Parte 3.9 *****/
 
 int compararTFechas(TFecha fecha1, TFecha fecha2)
 {
-    int res = 0;
-    /************ Parte 3.10 ************/
-    /*Escriba el código a continuación */
-
-    /****** Fin de parte Parte 3.10 *****/
-    return res;
+    //imagino que las fechas no son nulas, si no aqui deberia ir el if de comprobacion de fechas nulas
+    return comparador(fecha1, fecha2);
 }
